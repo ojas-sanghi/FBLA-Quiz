12008122fb57c6530fd1ee5e35117cc8a64dce2a
Ojas Sanghi <ojas.sanghi@protonmail.com>
2020-12-01 00:34:18 -0700

    Design fill-in-blank question, half-design matching question

--- kivy-code/FBLAQuizApp.py
+++ kivy-code/FBLAQuizApp.py
@@ -1,14 +1,19 @@
 
 from kivy.lang import Builder
-from kivy.uix.screenmanager import ScreenManager, Screen, SwapTransition
+from kivy.uix.screenmanager import ScreenManager, Screen
 
 from kivymd.app import MDApp
-
 
 class MCQScreen(Screen):
     pass
 
 class TFScreen(Screen):
+    pass
+
+class BlankScreen(Screen):
+    pass
+
+class MatchingScreen(Screen):
     pass
 
 class ScreenManagement(ScreenManager):
@@ -24,6 +29,7 @@ class FBLAQuizApp(MDApp):
 
     def __init__(self, **kwargs) -> None:
         super(FBLAQuizApp, self).__init__(**kwargs)
+
     
     def build(self):
         self.set_text()
@@ -32,11 +38,11 @@ class FBLAQuizApp(MDApp):
         self.theme_cls.theme_style = "Light"
     
     def set_text(self):
-        self.question = "fbla full form"
-        self.option_1 = "future dumb"
-        self.option_2 = "future smart"
-        self.option_3 = "future scammers"
-        self.option_4 = "future leaders"
+        self.question = "this is a multiple choice question"
+        self.option_1 = "option 1"
+        self.option_2 = "option 2"
+        self.option_3 = "option 3"
+        self.option_4 = "option 4"
 
     def select(self, param):
         print(param)
