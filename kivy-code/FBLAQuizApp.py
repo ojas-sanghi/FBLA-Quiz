37cb1e3f41ffc761ee318ca6e3ad04076239ab16
Ojas Sanghi <ojas.sanghi@protonmail.com>
2020-12-03 21:42:35 -0700

    Separate classes out into files, move label into Utils.kv

--- kivy-code/FBLAQuizApp.py
+++ kivy-code/FBLAQuizApp.py
@@ -1,47 +1,23 @@
-
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.clock import Clock
-from kivy.properties import StringProperty
 
 from kivymd.app import MDApp
+# from kivymd.uix.button import MDRaisedButton
 
-from matching import MatchingQuestion
+from mcq import MCQScreen
+from tf import TFScreen
+from blank import BlankScreen
+from matching import MatchingScreen
+from checkbox import CheckboxScreen
+from saq import SAQScreen
 
-class MCQScreen(Screen):
-    question = StringProperty("this is a multiple choice question")
-
-    option_1 = "option 1"
-    option_2 = "option 2"
-    option_3 = "option 3"
-    option_4 = "option 4"
-
-    def select(self, param):
-        print(param)
-        self.question = "cool MCQ question"
-
-        print(self.ids)
     
-    ###
-    # for the dropdownmenu thing
-    # maybe define that part in this file,
-    # then add_widget(Builder.load_string)
-    # or i guess Builder.load_file might work too
-
-class TFScreen(Screen):
-    pass
-
-class BlankScreen(Screen):
-    pass
-
-class MatchingScreen(Screen):
-    pass
-
-class CheckboxScreen(Screen):
-    pass
-
-class SAQScreen(Screen):
-    pass
+###
+# for the MDDropdownMenu thing
+# maybe define that part in this file,
+# then add_widget(Builder.load_string)
+# or i guess Builder.load_file might work too
 
 class ScreenManagement(ScreenManager):
     pass
@@ -58,13 +34,10 @@ class FBLAQuizApp(MDApp):
     def __init__(self, **kwargs) -> None:
         super().__init__(**kwargs)
 
-        Builder.load_file("utils.kv")
-        Builder.load_file("mcq.kv")
-        Builder.load_file("tf.kv")
-        Builder.load_file("blank.kv")
-        Builder.load_file("matching.kv")
-        Builder.load_file("checkbox.kv")
-        Builder.load_file("saq.kv")
+        file_names = ["utils.kv", "mcq.kv", "tf.kv", "blank.kv", "matching.kv", "checkbox.kv", "saq.kv"]
+
+        for file in file_names:
+            Builder.load_file(file)
 
         Clock.schedule_once(self._late_init)
 
@@ -74,16 +47,15 @@ class FBLAQuizApp(MDApp):
 
     def build(self):
         self.root = Builder.load_file("FBLAQuizApp.kv")
-        self.theme_cls.theme_style = "Light"
+        self.theme_cls.theme_style = "Dark"
 
 
     def select(self, param):
         print(param)
         self.question = "asdasda"
     
-    def switch_match(self):
-        self.root.current = "Matching"
-        MatchingQuestion.do_stuff(self)
+    def matching_select(self, dropdown):
+        MatchingScreen.matching_select(self.root.current_screen, dropdown)
         
 if __name__ == "__main__":
     FBLAQuizApp().run()
---
+++ kivy-code/tf.py
@@ -0,0 +1,4 @@
+from kivy.uix.screenmanager import Screen
+
+class TFScreen(Screen):
+    pass
